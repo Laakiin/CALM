a28fe3e2c70d2b892e0b85054fc2058a09b4a603
anakinmrq <anakinmrq@gmail.com>
2021-10-14 18:44:45 +0200

    Closing that FUCKING CASE WITH ESAC

--- pacman-CALM.sh
+++ pacman-CALM.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 homedir="/home/$1"
-
-
-
 sudo useradd -m -U $1
 sudo passwd $1
-sudo su $1 -c "bash scripts/pacmanInstall.sh"
+sudo su $1 -c "bash scripts/pacmanInstall.sh $1"
--- scripts/pacmanInstall.sh
+++ scripts/pacmanInstall.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+homedir="/home/$1"
 echo "Installing i3-wm"
 sudo pacman -S i3-wm --noconfirm
 if [ $? -eq 0 ];
@@ -8,6 +10,7 @@ then
 	case $yn in
 		[Yy]* ) sudo pacman -S polybar && sudo rm -r $homedir/.config/polybar/blocks $homedir/.config/polybar/colorblocks $homedir/.config/polybar/cuts $homedir/.config/polybar/docky $homedir/.config/polybar/forest $homedir/.config/polybar/grayblocks $homedir/.config/polybar/hack $homedir/.config/polybar/material $homedir/.config/polybar/shades $homedir/.config/polybar/shapes && sudo cp -r res/i3/customTheme_Shapes shapes && cp res/i3/i3conf_polybar $homedir/.config/i3/config;;
 		[Nn]* ) cp res/i3/i3conf_nopolybar $homedir/.config/i3/config;;
+	esac
 
 else
 	echo "Error while installing i3-wm"
