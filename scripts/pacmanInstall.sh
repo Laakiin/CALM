a5938e05f03779f6fde79545968dd1add985bcb1
Laakiin <anakinmrq@gmail.com>
2021-11-17 10:26:22 +0100

    Polybar Removal

--- scripts/pacmanInstall.sh
+++ scripts/pacmanInstall.sh
@@ -4,14 +4,8 @@ homedir="/home/$1"
 echo "Installing i3-wm"
 sudo -S pacman -S i3-wm
 if [ $? -eq 0 ];
-then
-	echo  "Do you want to install polybar?" 
-	read -p "You might need to edit config files for your computer" yn
-	case $yn in
-		[Yy]* ) sudo -S pacman -S polybar && sudo -S rm -r $homedir/.config/polybar/blocks $homedir/.config/polybar/colorblocks $homedir/.config/polybar/cuts $homedir/.config/polybar/docky $homedir/.config/polybar/forest $homedir/.config/polybar/grayblocks $homedir/.config/polybar/hack $homedir/.config/polybar/material $homedir/.config/polybar/shades $homedir/.config/polybar/shapes && sudo -S cp -r res/i3/customTheme_Shapes shapes && cp res/i3/i3conf_polybar $homedir/.config/i3/config;;
-		[Nn]* ) cp res/i3/i3conf_nopolybar $homedir/.config/i3/config;;
-	esac
-
+then 
+	cp res/i3/i3conf $homedir/.config/i3/config
 else
 	echo "Error while installing i3-wm"
 fi
