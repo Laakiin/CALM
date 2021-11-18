77e28fafb36fcf7a549ac3d3e8dc058e778cd35d
Laakiin <anakinmrq@gmail.com>
2021-11-18 08:58:28 +0100

    Res folder in /usr/share/CALM

--- pacman-CALM.sh
+++ pacman-CALM.sh
@@ -6,7 +6,7 @@ if [[ -n $1 ]]; then
     echo "Now you have to set a password for $1: "
     sudo passwd $1
     sudo usermod -aG wheel $1
-    sudo cp -r res $homedir
+    sudo mv ./res /usr/share/CALM/res/
     sudo su $1 -c "bash scripts/pacmanInstall.sh $1"
 else
     echo "You have to set the username of your new accout as an argument!"
--- scripts/pacmanInstall.sh
+++ scripts/pacmanInstall.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 homedir="/home/$1"
+resdir="/usr/share/CALM/res"
 cd $homedir
 mkdir .config/i3/
 echo "Installing i3-wm"
 sudo -S pacman -S i3-wm
 if [ $? -eq 0 ];
 then 
-	sudo -S cp res/i3/i3conf $homedir/.config/i3/config
+	sudo -S cp $resdir/i3/i3conf $homedir/.config/i3/config
     bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
     rm $homedir/.bashrc
-    sudo -S cp res/bashrc $homedir/.bashrc
+    sudo -S cp $resdir/bashrc $homedir/.bashrc
     rm -r $homedir/.oh-my-bash/themes/brainy
-    sudo -S cp -r res/ohmybash/customTheme_Brainy $homedir/.oh-my-bash/themes/brainy
-    sudo -S cp -r res/scripts $homedir/.scripts
+    sudo -S cp -r $resdir/ohmybash/customTheme_Brainy $homedir/.oh-my-bash/themes/brainy
+    sudo -S cp -r $resdir/scripts $homedir/.scripts
 else
 	echo "Error while installing i3-wm"
 fi
