fd462c3858c46ea59b5600265e7b69eeb88e5bbc
Laakiin <anakinmrq@gmail.com>
2021-11-18 16:58:53 +0100

    Final addons

--- scripts/pacmanInstall.sh
+++ scripts/pacmanInstall.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 YEL='\033[1;33m'
+BLU='\033[0;34m'
 RED='\033[0;31m'
 NC='\033[0m'
 
@@ -8,9 +9,9 @@ homedir="/home/$1"
 resdir="/usr/share/CALM/res"
 cd $homedir
 mkdir .config/i3/
-echo -e "\n${YEL}####Installing i3, xtrlock, flameshot, firefox, thunar, tilix####\n
+echo -e "\n${YEL}####Installing i3, xtrlock, flameshot, firefox, vim, thunar, tilix####\n
 ####(If you don't know what you have to type, type '2,5')####${NC}\n"
-sudo -S pacman -Syu i3 xtrlock tilix flameshot thunar firefox
+sudo -S pacman -Syu i3 xtrlock tilix flameshot thunar firefox vim
 if [ $? -eq 0 ];
 then
     echo -e "\n${YEL}####Copying i3 config####\n" 
@@ -27,6 +28,14 @@ then
     sudo -S cp -r $resdir/scripts $homedir/.scripts
     echo -e "\n${YEL}####Rights adjustment####\n"
     sudo chmod u+rw .bashrc .scripts/* .config/i3/config
+    echo -e "\n${YEL}####Installing SebDruon's vim config####\n"
+    git clone https://github.com/GlaconBleu/config_vim.git ~/.config_vim
+    ln -s .config_vim .vim
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    echo -e "\n${BLU}####Now, you have to start vim with 'vim' ####\n
+then you type: 'VundleInstall'.\n
+    You wait that the installation finishes, and finally you can leave vim with ':q!'\n${NC}"
+
 else
-	echo -e "\n${RED}####Error while installing i3, xtrlock, firefox, flameshot tilix####\n"
+	echo -e "\n${RED}####Error while installing i3, xtrlock, firefox, vim, flameshot tilix####\n"
 fi
