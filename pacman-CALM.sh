bf897a8cce6c3a6f31f8610b19791e4dcd40d11b
Laakiin <anakinmrq@gmail.com>
2021-11-18 09:14:43 +0100

    Adding state echos

--- pacman-CALM.sh
+++ pacman-CALM.sh
@@ -5,9 +5,12 @@ if [[ -n $1 ]]; then
     sudo useradd -m -U $1
     echo "Now you have to set a password for $1: "
     sudo passwd $1
+    echo "Adding $1 to sudoers"
     sudo usermod -aG wheel $1
+    echo "Creating and moving res file to /usr/share/CALM"
     sudo mkdir /usr/share/CALM
     sudo mv ./res /usr/share/CALM/res/
+    echo "Launching the installation"
     sudo su $1 -c "bash scripts/pacmanInstall.sh $1"
 else
     echo "You have to set the username of your new accout as an argument!"
--- scripts/pacmanInstall.sh
+++ scripts/pacmanInstall.sh
@@ -7,13 +7,18 @@ mkdir .config/i3/
 echo "Installing i3-wm"
 sudo -S pacman -S i3-wm
 if [ $? -eq 0 ];
-then 
+then
+    echo "Copying i3 config" 
 	sudo -S cp $resdir/i3/i3conf $homedir/.config/i3/config
+    echo "Installing oh-my-bash"
     bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+    echo "Deleting .bashrc"
     rm $homedir/.bashrc
+    echo "Copying the new .bashrc"
     sudo -S cp $resdir/bashrc $homedir/.bashrc
-    rm -r $homedir/.oh-my-bash/themes/brainy
-    sudo -S cp -r $resdir/ohmybash/customTheme_Brainy $homedir/.oh-my-bash/themes/brainy
+    echo "Copying new brainy theme"
+    sudo -S cp -r $resdir/ohmybash/brainy_alt $homedir/.oh-my-bash/themes/
+    echo "Copying scripts directory"
     sudo -S cp -r $resdir/scripts $homedir/.scripts
 else
 	echo "Error while installing i3-wm"
